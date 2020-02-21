a2c207980a3a65f70162cfb5785d05bf2925a291
Coop Williams <ken.c.williams.iii@gmail.com>
2020-02-21 02:41:48 -0500

    add some types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let _x = 2.0;
+
+    let y: f32 = 3.0;
+
+    let x = 5;
+    
+    let x = x +1;
+
+    let x = x * 2;
+
+    println!("The value of x,y is: {},{}", x, y);
 }

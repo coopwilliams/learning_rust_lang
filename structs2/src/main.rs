798ffd9f2be7432f4a1fb9208b2c04e2ec48d448
coopwilliams <ken.c.williams.iii@gmail.com>
2020-03-17 10:36:44 -0400

    add method and assoc. func

--- structs2/src/main.rs
+++ structs2/src/main.rs
@@ -3,12 +3,18 @@ fn main() {
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
 
     println!(
         "rect1 is {:#?}", rect1
     );
+
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
 #[derive(Debug)]
@@ -17,6 +23,17 @@ struct Rectangle {
     height: u32,
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    // associated function is not a method because it doesn't take &self 
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }

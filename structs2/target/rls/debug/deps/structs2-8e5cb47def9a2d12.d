e92a321198bfa181447db0aa7cc0b6cdc9df6cb6
coopwilliams <ken.c.williams.iii@gmail.com>
2020-03-17 00:54:44 -0400

    make rectangle as struct

---
+++ structs2/target/rls/debug/deps/structs2-8e5cb47def9a2d12.d
@@ -0,0 +1,5 @@
+c:\Users\Cooper\learning\learning_rust_lang\structs2\target\rls\debug\deps\structs2-8e5cb47def9a2d12.rmeta: src\main.rs
+
+c:\Users\Cooper\learning\learning_rust_lang\structs2\target\rls\debug\deps\structs2-8e5cb47def9a2d12.d: src\main.rs
+
+src\main.rs:
---
+++ structs2/target/rls/debug/deps/structs2-f2a7be5090f3435c.d
@@ -0,0 +1,5 @@
+c:\Users\Cooper\learning\learning_rust_lang\structs2\target\rls\debug\deps\structs2-f2a7be5090f3435c.rmeta: src\main.rs
+
+c:\Users\Cooper\learning\learning_rust_lang\structs2\target\rls\debug\deps\structs2-f2a7be5090f3435c.d: src\main.rs
+
+src\main.rs:

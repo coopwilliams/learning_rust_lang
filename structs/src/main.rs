05c81e23cc2a522583620ee20884d7900543dbfd
coopwilliams <ken.c.williams.iii@gmail.com>
2023-09-08 12:12:45 -0400

    test field permissions

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -12,11 +12,12 @@ fn main() {
         email: String::from("clunk@bunk.com"),
         username: String::from("clunk"),
         ..user1
-    }
+    };
 
     let black = Color(0, 0, 0);
     let origin = Point(0, 0, 0);
 
+    test_field_level_permissions();
     
 }
 
@@ -31,7 +32,7 @@ struct Color(i32, i32, i32);
 struct Point(i32, i32, i32);
 
 
-fn build_user(email: String, username: String) => User {
+fn build_user(email: String, username: String) -> User {
     User {
         email, // we can use field init shorthand by giving params the same names as fields
         username,
@@ -40,3 +41,12 @@ fn build_user(email: String, username: String) => User {
     }
 }
 
+fn test_field_level_permissions() {
+    struct Point { x: i32, y: i32 }
+    
+    let mut p = Point { x: 0, y: 0 };
+    let x = &mut p.x;
+    p.y = 5; // We can still write this even though p.x is borrowed.
+    *x += 1;
+    println!("{}, {}", p.x, p.y);
+    }

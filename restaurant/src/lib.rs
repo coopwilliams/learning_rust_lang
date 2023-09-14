737e56fffb90e5dd9175fec2c47045b5ae39aa1e
coopwilliams <ken.c.williams.iii@gmail.com>
2023-09-14 14:42:50 -0400

    separating modules out

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -13,23 +13,11 @@ mod tests {
     }
 }
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
 
 fn deliver_order() {}
+
+// moved to its own file
+mod front_of_house;
 
 mod back_of_house {
     // making an enum public
@@ -64,12 +52,9 @@ mod back_of_house {
     fn cook_order() {}
 }
 
-use crate::front_of_house::hosting;
+use front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
-    crate::front_of_house::hosting::add_to_waitlist();
-
-    front_of_house::hosting::add_to_waitlist();
 
     // hosting is a valid name in scope
     // because of "use" of 'hosting' in

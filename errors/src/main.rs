0cc503d9e5bdc21986c0cb496de4bc53957e3c86
coopwilliams <ken.c.williams.iii@gmail.com>
2023-09-21 11:48:57 -0400

    more error handling

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::fs::File;
 use std::io::{self, Read, ErrorKind};
 
@@ -17,11 +18,19 @@ fn main() {
         },
     };
 
+    // all these functions do the same thing in progressively
+    // shorter ways.
     let username1 = read_username_from_file_long_way();
     println!("{:?}", username1);
     let username2 = read_username_from_file_short_way();
     println!("{:?}", username2);
+    let username3 = read_username_from_file_shorter_way();
+    println!("{:?}", username3);
+    let username4 = read_username_from_file_shortest_way();
+    println!("{:?}", username4);
 
+    let last_char = last_char_of_first_line(&username3.unwrap());
+    println!("{:?}", last_char.unwrap());
 }
 
 fn read_username_from_file_long_way() -> Result<String, io::Error> {
@@ -45,4 +54,19 @@ fn read_username_from_file_short_way() -> Result<String, io::Error> {
     let mut username = String::new();
     username_file.read_to_string(&mut username)?;
     Ok(username)
+}
+
+fn read_username_from_file_shorter_way() -> Result<String, io::Error> {
+    let mut username = String::new();
+    File::open("hello4.txt")?.read_to_string(&mut username)?;
+
+    Ok(username)
+}
+
+fn read_username_from_file_shortest_way() -> Result<String, io::Error> {
+    fs::read_to_string("hello5.txt")
+}
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
 }

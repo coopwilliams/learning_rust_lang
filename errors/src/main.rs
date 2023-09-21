93a4e0902897047c42e51e8d6a475741ecf7209f
coopwilliams <ken.c.williams.iii@gmail.com>
2023-09-21 10:56:59 -0400

    propagating errors

---
+++ errors/src/main.rs
@@ -0,0 +1,48 @@
+use std::fs::File;
+use std::io::{self, Read, ErrorKind};
+
+fn main() {
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+            }
+        },
+    };
+
+    let username1 = read_username_from_file_long_way();
+    println!("{:?}", username1);
+    let username2 = read_username_from_file_short_way();
+    println!("{:?}", username2);
+
+}
+
+fn read_username_from_file_long_way() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello2.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file_short_way() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello3.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}

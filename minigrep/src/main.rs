6123ec0d23b9cd8ac6ea4c1077f0d350b343ec7c
coopwilliams <ken.c.williams.iii@gmail.com>
2023-09-25 20:15:33 -0400

    start minigrep, fileread

---
+++ minigrep/src/main.rs
@@ -0,0 +1,18 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    // parse args
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
+ 
+    // read the file
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
+}

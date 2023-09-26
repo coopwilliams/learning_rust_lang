8c67f3b76139150327fe32b77548b44ff48cbc39
coopwilliams <ken.c.williams.iii@gmail.com>
2023-09-25 20:40:07 -0400

    refactor pt 1

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,17 +2,30 @@ use std::env;
 use std::fs;
 
 fn main() {
+
+    // according to general convention,
+    // the responsibilities of the main()
+    // function in a complicated program are:
+
+    // - Calling the command line parsing logic with the argument values
+    // - Setting up any other configuration
+    // - Calling a run function in lib.rs
+    // - Handling the error if run returns an error
+
     // parse args
     let args: Vec<String> = env::args().collect();
+    let (query, file_path) = parse_config(&args);
+
+    // read the file
+    let contents = fs::read_to_string(file_path)
+    .expect("Should have been able to read the file");
+
+println!("With text:\n{contents}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
     let query = &args[1];
     let file_path = &args[2];
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
- 
-    // read the file
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
+    (query, file_path)
 }

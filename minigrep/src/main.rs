7a6e1bedd70d028f40bddfa79e14e700de884daa
coopwilliams <ken.c.williams.iii@gmail.com>
2023-09-25 20:43:38 -0400

    minigrep refactor w/ Config

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -14,18 +14,23 @@ fn main() {
 
     // parse args
     let args: Vec<String> = env::args().collect();
-    let (query, file_path) = parse_config(&args);
+    let config = parse_config(&args);
 
     // read the file
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
     .expect("Should have been able to read the file");
 
 println!("With text:\n{contents}");
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
+struct Config {
+    query: String,
+    file_path : String,
+}
 
-    (query, file_path)
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config {query, file_path}
 }

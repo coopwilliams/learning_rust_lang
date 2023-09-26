9b0004f256664894f34f8dc78618bab67b31521f
coopwilliams <ken.c.williams.iii@gmail.com>
2023-09-25 21:17:48 -0400

    minigrep refactor: call lib.rs from main.rs

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,28 @@
+use std::{fs, error::Error};
+
+pub struct Config {
+    query: String,
+    file_path : String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough args");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        
+        Ok(Config {query, file_path})
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // read the file
+    let contents = fs::read_to_string(config.file_path)?;
+        // .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
+
+    Ok(())
+}
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,6 @@
-use std::{env, fs, process};
+use std::{env, fs, process, error::Error};
+
+use minigrep::Config;
 
 fn main() {
 
@@ -18,26 +20,8 @@ fn main() {
         process::exit(1);
     });
 
-    // read the file
-    let contents = fs::read_to_string(config.file_path)
-    .expect("Should have been able to read the file");
-
-println!("With text:\n{contents}");
-}
-
-struct Config {
-    query: String,
-    file_path : String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough args");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        
-        Ok(Config {query, file_path})
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }

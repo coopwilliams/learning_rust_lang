03d4220a56e2195756b29e67ae4b2850bc75a31b
coopwilliams <ken.c.williams.iii@gmail.com>
2023-09-25 22:09:12 -0400

    add error msgs back, direct to stderr

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -15,11 +15,13 @@ fn main() {
 
     // parse args
     let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|_err| {
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-
-    if let Err(_e) = minigrep::run(config) {
+    
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }

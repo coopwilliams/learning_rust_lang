f155aef5b45b47b71131053bfc52e629e4369cfd
coopwilliams <ken.c.williams.iii@gmail.com>
2023-09-25 21:33:10 -0400

    write, test a search() function

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -22,7 +22,36 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
         // .expect("Should have been able to read the file");
 
-    println!("With text:\n{contents}");
-
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line)
+        }
+    }
+    
+    results
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        // huh. This compiles even if 
+        // search() doesn't exist yet.
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -16,12 +16,10 @@ fn main() {
     // parse args
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing args: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
         process::exit(1);
     }
 }

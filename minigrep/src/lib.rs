d4cc9111ab7b0d3a7e75c2fb9f191ed47877b20b
coopwilliams <ken.c.williams.iii@gmail.com>
2023-10-03 15:15:52 -0400

    speedup Config.build() w iterators

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -7,17 +7,26 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough args");
+    pub fn build(
+            mut args: impl Iterator<Item = String>, 
+        ) -> Result<Config, &'static str> {
+            
+            args.next();
+
+            let query = match args.next() {
+                Some(arg) => arg,
+                None => return Err("Didn't get a query string"),
+            };
+
+            let file_path = match args.next() {
+                Some(arg) => arg,
+                None => return Err("Didn't get a file path"),
+            };
+
+            let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+            Ok(Config {query, file_path, ignore_case})
         }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        let ignore_case = env::var("IGNORE_CASE").is_ok();
-
-        Ok(Config {query, file_path, ignore_case})
-    }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -14,8 +14,7 @@ fn main() {
     // - Handling the error if run returns an error
 
     // parse args
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });

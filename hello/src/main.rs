cb5b383601120af6a0f84a008f32d93c3ae62c80
coopwilliams <ken.c.williams.iii@gmail.com>
2023-10-31 13:36:43 -0400

    send html in response

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     net::{TcpStream, TcpListener}, 
     io::{prelude::*, BufReader}
 };
@@ -22,7 +23,12 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+    let response = 
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     // write_all() takes &[u8] and sends those bytes down the connection.
     stream.write_all(response.as_bytes()).unwrap();

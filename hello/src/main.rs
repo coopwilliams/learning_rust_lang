a938ccee8a86d9b935122d1d7fc929f83f81755b
coopwilliams <ken.c.williams.iii@gmail.com>
2023-10-31 19:31:05 -0400

    simulate slow request to motivate multithreading

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,7 +1,9 @@
 use std::{
     fs,
     net::{TcpStream, TcpListener}, 
-    io::{prelude::*, BufReader}
+    io::{prelude::*, BufReader},
+    thread,
+    time::Duration,
 };
 
 fn main() {
@@ -18,10 +20,13 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "404.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
     
     let contents = fs::read_to_string(filename).unwrap();

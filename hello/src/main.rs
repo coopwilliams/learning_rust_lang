1e66041577dd5420964abd00f2cfcd3375bf2fb6
coopwilliams <ken.c.williams.iii@gmail.com>
2023-10-31 13:25:46 -0400

    handle connections

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,4 +1,7 @@
-use std::net::TcpListener;
+use std::{
+    net::{TcpStream, TcpListener}, 
+    io::{prelude::*, BufReader}
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -6,6 +9,18 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        // The browser signals the end of a request by sending \n\n
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
 }
